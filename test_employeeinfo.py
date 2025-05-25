5180c3127aae2c2690f93da9eacc83f88d3d953b
LeiShweSin <leishwe.24@ichat.sp.edu.sg>
2025-05-25 20:16:03 +0800

    added employee info test file

--- test_employeeinfo.py
+++ test_employeeinfo.py
@@ -7,3 +7,17 @@ def test_age_range():
     test = [{"name": "Chloe",  "age": 35, "department": "Engineering", "salary": 70000},
     {"name": "Mike", "age": 32, "department": "Engineering", "salary": 65000}]
     assert result==test
+
+def test_calculate_average_salary():
+    result = emp.calculate_average_salary()
+    assert(result == 60166.67) 
+
+def test_get_employees_by_dept():
+    test = [
+        {"name": "John", "age": 30, "department": "Sales", "salary": 50000},
+        {"name": "Peter", "age": 40, "department": "Sales", "salary": 60000}
+    ]
+    result = emp.get_employees_by_dept('Sales') 
+
+    assert(result==test)  # Assuming the expected result is correct, adjust as per your data  
+    
